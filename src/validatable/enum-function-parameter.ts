e46206a1521f2f63252b563dab1c1b200b7cd28e
dikac <rigel234@yahoo.com>
2022-02-05 01:33:47 +0700

    change type for return to return

--- src/validatable/enum-function-parameter.ts
+++ src/validatable/enum-function-parameter.ts
@@ -1,5 +1,5 @@
 import EnumParameter from '../assert/string/enum-parameter';
-import EnumFunctionParameters, {Type} from './enum-function-parameters';
+import EnumFunctionParameters, {EnumFunctionParametersReturn} from './enum-function-parameters';
 import Message from '@alirya/message/message';
 import {EnumClassArgument, EnumClassArgumentMessage} from './enum-class-parameter';
 
@@ -9,14 +9,14 @@ export default function EnumFunctionParameter<MessageType, Enumerate extends Rec
         enumerate,
         message,
     } : EnumClassArgument<Enumerate, Argument> & Message<EnumClassArgumentMessage<MessageType, Enumerate>>
-) :  Type<MessageType, Enumerate, Argument>;
+) :  EnumFunctionParametersReturn<MessageType, Enumerate, Argument>;
 
 export default function EnumFunctionParameter<Enumerate extends Record<string, any>, Argument extends unknown>(
     {
         value,
         enumerate,
     } : EnumClassArgument<Enumerate, Argument>
-) :  Type<string, Enumerate, Argument>;
+) :  EnumFunctionParametersReturn<string, Enumerate, Argument>;
 
 
 export default function EnumFunctionParameter<MessageType, Enumerate extends Record<string, any>, Argument extends unknown>(
@@ -25,7 +25,7 @@ export default function EnumFunctionParameter<MessageType, Enumerate extends Rec
         enumerate,
         message = EnumParameter,
     } : EnumClassArgument<Enumerate, Argument> & Partial<Message<EnumClassArgumentMessage<MessageType|string, Enumerate>>>
-) :  Type<MessageType|string, Enumerate, Argument> {
+) :  EnumFunctionParametersReturn<MessageType|string, Enumerate, Argument> {
 
     return EnumFunctionParameters(
         value,
--- src/validatable/enum-function-parameters.ts
+++ src/validatable/enum-function-parameters.ts
@@ -4,26 +4,26 @@ import ValidatorReturn from '@alirya/validator/validatable/simple';
 import EnumParameters from '../assert/string/enum-parameters';
 
 
-export type Type<Message, Enumerate extends Record<string, any>, Argument extends unknown> =
+export type EnumFunctionParametersReturn<Message, Enumerate extends Record<string, any>, Argument extends unknown> =
     ValidatorReturn<Argument, Object.UnionOf<Enumerate>, EnumClassParameters<Message, Enumerate, Argument>> & {enumerate : Enumerate};
 
 export default function EnumFunctionParameters<Message, Enumerate extends object, Argument extends unknown>(
     value : Argument,
     enumerate : Enumerate,
     message : EnumClassArgumentMessage<Message, Enumerate>
-) :  Type<Message, Enumerate, Argument>;
+) :  EnumFunctionParametersReturn<Message, Enumerate, Argument>;
 
 export default function EnumFunctionParameters<Enumerate extends Record<string, any>, Argument extends unknown>(
     value : Argument,
     enumerate : Enumerate,
-) :  Type<string, Enumerate, Argument>;
+) :  EnumFunctionParametersReturn<string, Enumerate, Argument>;
 
 
 export default function EnumFunctionParameters<Message, Enumerate extends Record<string, any>, Argument extends unknown>(
     value : Argument,
     enumerate : Enumerate,
     message : EnumClassArgumentMessage<Message|string, Enumerate> = EnumParameters
-) :  Type<Message, Enumerate, Argument> {
+) :  EnumFunctionParametersReturn<Message, Enumerate, Argument> {
 
-    return <Type<Message, Enumerate, Argument>> new EnumClassParameters(value, enumerate, message);
+    return <EnumFunctionParametersReturn<Message, Enumerate, Argument>> new EnumClassParameters(value, enumerate, message);
 }
